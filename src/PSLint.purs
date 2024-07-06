cc9b7aad62c4a0eca39df28e17f5b21d3568b660
Avinash Verma <avinashverma@Avinashs-MacBook-Pro.local>
2024-07-06 21:15:16 +0530

    init commit

---
+++ src/PSLint.purs
@@ -0,0 +1,59 @@
+module PSLint where
+
+import Data.Tuple.Nested (type (/\), (/\))
+import Prelude
+
+import Data.Array.NonEmpty as NArray
+import Data.Either (Either(..))
+import Data.Set as S
+import Data.Traversable (traverse)
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Node.Buffer.Class as Buffer
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readFile)
+import Node.Glob.Basic (expandGlobsCwd)
+import PureScript.CST (RecoveredParserResult(..), parseModule)
+import PureScript.CST.Errors (printParseError)
+import PureScript.CST.Traversal (foldMapModule)
+import PureScript.CST.Types as CST
+
+
+type PSLintConfig = {
+  src :: String, -- "src/**/*.purs"
+  lintModule :: CST.Module Void -> (Array (String /\ CST.SourceRange)),
+  lintDecl :: CST.Declaration Void -> (Array (String /\ CST.SourceRange)),
+  lintBinder :: CST.Binder Void -> (Array (String /\ CST.SourceRange)),
+  lintExpr :: CST.Expr Void -> (Array (String /\ CST.SourceRange)),
+  lintType :: CST.Type Void -> (Array (String /\ CST.SourceRange))
+}
+
+runPSLint :: PSLintConfig -> (Either String (Array (String /\ CST.SourceRange)) -> Effect Unit) -> Effect Unit
+runPSLint config fn = launchAff_ $ do 
+    files :: Array String <- (pure <<< S.toUnfoldable) =<< expandGlobsCwd [ config.src ]
+    if files == []
+      then liftEffect $ fn (Left ("No Files found with src : " <> config.src))
+      else do
+        _ <-traverse (\file_path -> do 
+            content <- (liftEffect <<< Buffer.toString UTF8) =<< readFile file_path
+            liftEffect $ fn $ Right $ lintModule content config
+            pure unit
+            ) files
+        pure unit
+
+
+
+lintModule :: String -> PSLintConfig -> Array (String /\ CST.SourceRange)
+lintModule content config = 
+  case parseModule content of
+    ParseSucceeded m -> 
+      (config.lintModule m) <> 
+      (foldMapModule $ 
+          { onExpr : config.lintExpr
+          , onType : config.lintType
+          , onBinder : config.lintBinder
+          , onDecl : config.lintDecl
+          }) m
+    ParseSucceededWithErrors _ err -> map (\{error, position} -> (printParseError error) /\ { start : {line : position.line + 1, column : position.column + 1}, end : {line : position.line + 1, column : position.column + 1}}) $ NArray.toArray err
+    ParseFailed {error, position} -> [(printParseError error) /\ { start : {line : position.line + 1, column : position.column + 1}, end : {line : position.line + 1, column : position.column + 1}}]
